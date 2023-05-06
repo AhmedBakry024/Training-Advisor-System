0005b0f1d0013e07418f8621d50486f00ecc8d12
Ahmed Bakry <ahmedbakry024@gmail.com>
2023-05-07 00:24:34 +0300

    Update VisitorAccount.java

--- VisitorAccount.java
+++ VisitorAccount.java
@@ -21,33 +21,33 @@ public class VisitorAccount {
             System.out.println("Enter new name");
             String nameString = scanner.next();
             Name = nameString;
-            System.out.println("Name changed successfully to (" + Name + " )");
+            System.out.println("Name changed successfully");
         }
         if (choice == 2) {
             System.out.println("Enter new email");
             String emailString = scanner.next();
             Email = emailString;
-            System.out.println("Email changed successfully to ( " + Email + " )");
+            System.out.println("Email changed successfully");
         }
         if (choice == 3) {
             System.out.println("Enter new password");
             String passwordString = scanner.next();
             Email = passwordString;
-            System.out.println("Password changed successfully to ( " + Pass + " )");
+            System.out.println("Password changed successfully");
         }
         if (choice == 4) {
             System.out.println("Enter new phone number");
             Integer phoneString = scanner.nextInt();
             phoneNum = phoneString;
-            System.out.println("Email changed successfully to ( " + phoneNum + " )");
+            System.out.println("Phone number changed successfully");
         }
 
     }
 
     public void viewdata() {
-        System.out.println("    Your profile"); 
-        System.out.println("    Your Name:  " + Name); 
-        System.out.println("    Your Email:  " + Email); 
-        System.out.println("    Your Phone Number:  " + phoneNum); 
+        System.out.println("    Your profile    "); 
+        System.out.println("Your Name:  " + Name); 
+        System.out.println("Your Email:  " + Email); 
+        System.out.println("Your Phone Number:  " + phoneNum); 
     }
 }
