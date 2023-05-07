244862d55dcb3c2c05d4c1d738d1a1cc7857cbd2
Ahmed Bakry <ahmedbakry024@gmail.com>
2023-05-07 21:14:35 +0300

    Update Course.java

--- Course.java
+++ Course.java
@@ -1,7 +1,6 @@
 import java.util.Vector;
 
-public class Course 
-{
+public class Course {
 
     private String courseName;
     private String coursetype;
@@ -11,19 +10,17 @@ public class Course
     private String prerequisites;
     public Exams exams;
     public Materials materials;
-  
 
-
-    public Course()
-    {
+    public Course() {
         this.courseName = "course Name";
         this.coursetype = "course type";
         this.courseID = "course ID";
         this.courseScore = 0;
         this.courseCost = 0;
     }
-    public Course(String name, String type, String ID,String pre, double cost,String material,String exam,String answer)
-    {
+
+    public Course(String name, String type, String ID, String pre, double cost, String material, String exam,
+            String answer) {
         this.courseName = name;
         this.coursetype = type;
         this.courseID = ID;
@@ -34,77 +31,78 @@ public class Course
         this.exams.setAnswersVector(answer);
     }
 
-    public void setCourse(String name, String type, String ID, int score, int cost)
-    {
+    public void setCourse(String name, String type, String ID, int score, int cost) {
         this.courseName = name;
         this.coursetype = type;
         this.courseID = ID;
         this.courseScore = score;
         this.courseCost = cost;
     }
-    public boolean Matching(String CourseName)
-    {
-        String name ;
-        for(int i =0;i<App.coursesVector.size();i++)
-        {
+
+    public boolean Matching(String CourseName) {
+        String name;
+        for (int i = 0; i < App.coursesVector.size(); i++) {
             name = App.coursesVector.get(i).getCourseName();
             name = name.toLowerCase();
-            if(name.equals(CourseName))
-            {
+            if (name.equals(CourseName)) {
                 return true;
             }
         }
         return false;
     }
-//Setters
-    public void setCourseName(String name)
-    {
+
+    //Setters
+    public void setCourseName(String name) {
         this.courseName = name;
     }
-    public void setCourseType(String type)
-    {
+
+    public void setCourseType(String type) {
         this.coursetype = type;
     }
-    public void setCourseID(String ID)
-    {
+
+    public void setCourseID(String ID) {
         this.courseID = ID;
     }
-    public void setCourseScore(Double score)
-    {
+
+    public void setCourseScore(Double score) {
         this.courseScore = score;
     }
-    public void setCourseCost(Double cost)
-    {
+
+    public void setCourseCost(Double cost) {
         this.courseCost = cost;
     }
-    public void setPrerequisites(String pre)
-    {
+
+    public void setPrerequisites(String pre) {
         this.prerequisites = pre;
     }
-    
-//Getters
-    public String getCourseName()
-    {
+
+    //Getters
+    public String getCourseName() {
         return this.courseName;
     }
-    public String getCourseType()
-    {
+
+    public String getCourseType() {
         return this.coursetype;
     }
-    public String getCourseID()
-    {
+
+    public String getCourseID() {
         return this.courseID;
     }
-    public double getCourseScore()
-    {
+
+    public double getCourseScore() {
         return this.courseScore;
     }
-    public double getCourseCost()
-    {
+
+    public double getCourseCost() {
         return this.courseCost;
     }
-    public String getPrerequisites()
-    {
+
+    public String getPrerequisites() {
         return this.prerequisites;
+    }
+
+    public void progressTracking() {
+
+        System.out.println("You finished " + courseScore + " % of the course");
     }
 }
